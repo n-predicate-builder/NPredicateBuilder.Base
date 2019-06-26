f9e3bdb93cc10659f767c56bd9e5a56dd6ae1537
Michael Bradvica <mjbradvica@protonmail.com>
2019-06-26 13:25:27 -0700

    fixed wrong interface for projections

--- src/NPredicateBuilder/INPredicateBuilder.cs
+++ src/NPredicateBuilder/INPredicateBuilder.cs
@@ -20,7 +20,7 @@ namespace NPredicateBuilder
         IEnumerable<TResult> EntitiesEnumerable<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection) where TSource : class;
         IEnumerable<TResult> EntitiesEnumerable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         IEnumerable<TResult> EntitiesEnumerable<TSource, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer) where TSource : class;
-        IEnumerable<TResult> EntitiesEnumerable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
+        IEnumerable<TResult> EntitiesEnumerable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         IEnumerable<TResult> EntitiesEnumerable<TSource, TResult>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection) where TSource : class;
 
         IQueryable<TSource> EntitiesQueryable<TSource>(BaseQuery<TSource> query) where TSource : class;
@@ -29,7 +29,7 @@ namespace NPredicateBuilder
         IQueryable<TResult> EntitiesQueryable<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection) where TSource : class;
         IQueryable<TResult> EntitiesQueryable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         IQueryable<TResult> EntitiesQueryable<TSource, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer) where TSource : class;
-        IQueryable<TResult> EntitiesQueryable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
+        IQueryable<TResult> EntitiesQueryable<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         IQueryable<TResult> EntitiesQueryable<TSource, TResult>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection) where TSource : class;
 
         List<TSource> EntitiesList<TSource>(BaseQuery<TSource> query) where TSource : class;
@@ -38,7 +38,7 @@ namespace NPredicateBuilder
         List<TResult> EntitiesList<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection) where TSource : class;
         List<TResult> EntitiesList<TSource, TFinalized, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         List<TResult> EntitiesList<TSource, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer) where TSource : class;
-        List<TResult> EntitiesList<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
+        List<TResult> EntitiesList<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         List<TResult> EntitiesList<TSource, TResult>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection) where TSource : class;
 
         Task<List<TSource>> EntitiesListAsync<TSource>(BaseQuery<TSource> query) where TSource : class;
@@ -47,7 +47,7 @@ namespace NPredicateBuilder
         Task<List<TResult>> EntitiesListAsync<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection) where TSource : class;
         Task<List<TResult>> EntitiesListAsync<TSource, TFinalized, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         Task<List<TResult>> EntitiesListAsync<TSource, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer) where TSource : class;
-        Task<List<TResult>> EntitiesListAsync<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TResult> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
+        Task<List<TResult>> EntitiesListAsync<TSource, TFinalized, TResult>(BaseQuery<TSource> query, IMultipleFinalizer<TSource, TFinalized> finalizer, Expression<Func<TFinalized, TResult>> projection) where TSource : class;
         Task<List<TResult>> EntitiesListAsync<TSource, TResult>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection) where TSource : class;
     }
 }
