79d00a021babe46c8e2405c27d3f876dcfa723ab
Michael Bradvica <mjbradvica@protonmail.com>
2019-07-08 12:20:56 -0700

    added projection to single entities

--- src/NPredicateBuilder/IPredicateBuilder.cs
+++ src/NPredicateBuilder/IPredicateBuilder.cs
@@ -8,11 +8,11 @@ namespace NPredicateBuilder
 {
     public interface IPredicateBuilder
     {
-        TResult Entity<TSource, TResult>(BaseQuery<TSource> query, ISingleFinalizer<TSource, TResult> finalizer) where TSource : class;
-        TResult Entity<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, ISingleFinalizer<TSource, TResult> finalizer) where TSource : class;
+        TResult Entity<TSource, TResult, TFinalized>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection, ISingleFinalizer<TResult, TFinalized> finalizer) where TSource : class;
+        TResult Entity<TSource, TResult, TFinalized>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection, ISingleFinalizer<TResult, TFinalized> finalizer) where TSource : class;
 
-        Task<TResult> EntityAsync<TSource, TResult>(BaseQuery<TSource> query, ISingleAsyncFinalizer<TSource, TResult> finalizer) where TSource : class;
-        Task<TResult> EntityAsync<TSource, TResult>(BaseQuery<TSource> query, BaseOrder<TSource> order, ISingleAsyncFinalizer<TSource, TResult> finalizer) where TSource : class;
+        Task<TResult> EntityAsync<TSource, TResult, TFinalized>(BaseQuery<TSource> query, Expression<Func<TSource, TResult>> projection, ISingleAsyncFinalizer<TResult, TFinalized> finalizer) where TSource : class;
+        Task<TResult> EntityAsync<TSource, TResult, TFinalized>(BaseQuery<TSource> query, BaseOrder<TSource> order, Expression<Func<TSource, TResult>> projection, ISingleAsyncFinalizer<TResult, TFinalized> finalizer) where TSource : class;
 
         IEnumerable<TSource> EntitiesEnumerable<TSource>(BaseQuery<TSource> query) where TSource : class;
         IEnumerable<TSource> EntitiesEnumerable<TSource>(BaseQuery<TSource> query, BaseOrder<TSource> order) where TSource : class;
