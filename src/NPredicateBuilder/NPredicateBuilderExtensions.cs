c46bf00356986c69d405aecb2bcbe4e92ac9b410
michael_bradvica <michael.bradvica@abr.com>
2019-07-29 13:33:19 -0400

    updated version

--- src/NPredicateBuilder/NPredicateBuilderExtensions.cs
+++ src/NPredicateBuilder/NPredicateBuilderExtensions.cs
@@ -1,17 +1,28 @@
 ﻿using LinqKit;
+using System;
 using System.Linq;
 
 namespace NPredicateBuilder
 {
     public static class NPredicateBuilderExtensions
     {
+        /// <summary>
+        /// Applies a series of Where filters to an IQueryable interface.
+        /// </summary>
         public static IQueryable<T> NPredicateBuilderWhere<T>(this IQueryable<T> queryable, BaseQuery<T> baseQuery)
         {
+            if (baseQuery.SearchExpression == null) throw new ArgumentNullException("There must be at least one predicate to filter by.");
+
             return queryable.AsExpandable().Where(baseQuery.SearchExpression);
         }
 
+        /// <summary>
+        /// Applies a series of Orderers to an IQueryable interface.
+        /// </summary>
         public static IOrderedQueryable<T> NPredicateBuilderOrder<T>(this IQueryable<T> queryable, BaseOrder<T> baseOrder)
         {
+            if (baseOrder.FirstOrder == null) throw new ArgumentNullException("There must be an OrderBy or OrderBy Descending to order by.");
+
             var orderedEntities = baseOrder.FirstOrder.Order(queryable);
 
             foreach (var orderer in baseOrder.SecondaryOrders)
