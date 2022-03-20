6029dd5726171b10035453f89fa387363d974093
Michael Bradvica <mjbradvica@protonmail.com>
2022-03-20 11:41:26 -0700

    version bumps

--- source/NPredicateBuilder.Tests/NPredicateBuilderWhereIntegrationTests.cs
+++ source/NPredicateBuilder.Tests/NPredicateBuilderWhereIntegrationTests.cs
@@ -2,8 +2,8 @@
 {
     using System.Collections.Generic;
     using System.Linq;
+    using EF;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using NPredicateBuilder.EF;
 
     [TestClass]
     public class NPredicateBuilderWhereIntegrationTests
